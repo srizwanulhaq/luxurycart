ffdeab087791eb319e0a3046bac07dbe7ad88880
waqasnoor222 <waqasnoor222>
2024-01-30 17:23:37 +0500

    01

--- src/app/demo/view/ToursSlots/tour-slots-details/tour-slots-details.component.ts
+++ src/app/demo/view/ToursSlots/tour-slots-details/tour-slots-details.component.ts
@@ -1,15 +1,19 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { TourSlotsMainComponent } from '../tour-slots-main/tour-slots-main.component';
 import { PointSlotDto } from 'src/app/demo/domain/Dto/PointSlots/PointSlotDto';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { TourPointSlotService } from 'src/app/demo/service/tour-point-slot.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tour-slots-details',
   templateUrl: './tour-slots-details.component.html',
-  styleUrls: ['./tour-slots-details.component.scss']
+  styleUrls: ['./tour-slots-details.component.scss'],
+  providers:[MessageService,ConfirmationService]
 })
 export class TourSlotsDetailsComponent implements OnInit {
   private _details: PointSlotDto;
-  constructor(public main: TourSlotsMainComponent) { }
+  constructor(public main: TourSlotsMainComponent,private confirmService: ConfirmationService,private service: TourPointSlotService,private messageService: MessageService,) { }
   ngOnInit() {
     console.log(this._details, 'details');
   }
@@ -25,5 +29,37 @@ export class TourSlotsDetailsComponent implements OnInit {
   get details(): PointSlotDto {
     return this._details;
   }
-
+  onStatus(e, id) {
+    var active = e.checked;
+    this.confirmService.confirm({
+      message: "Do you want to change status?",
+      header: "Change Confirmation",
+      icon: "pi pi-info-circle",
+      accept: () => {
+        //-------------------------------------
+        var model = {
+          id: id,
+          active: active,
+        };
+        this.service
+          .changeStatus(model)
+          .pipe(first())
+          .subscribe({
+            next: (response) => {
+              this.eventChange.emit(response.status);
+              this.messageService.add({severity: 'success', summary: 'Successful', detail: response.message, life: 3000});
+              this.main.bottomPanelActive = false;
+            },
+            error: (error) => {
+                this.main.bottomPanelActive = true;
+                this.messageService.add({severity: 'error', summary: 'Failed', detail: error.error.message, life: 3000});
+            },
+          });
+   
+      },
+      reject: () => {
+        this.details.active = !active;
+      },
+    });
+  }
 }
