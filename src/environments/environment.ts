8a9318d0579dccb5acbcb25906ccd03076fa3025
waqasnoor222 <waqasnoor222>
2024-05-09 18:41:51 +0500

    01

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
     gooogleApiKey: "AIzaSyDJIqhX4P6Uc99HxWRow9CljXntIQ-Od0g",
-    //apiUrl: 'https://panel.lcprod.link',
-    apiUrl: 'https://localhost:44369',
+    apiUrl: 'https://panel.lcprod.link',
+    // apiUrl: 'https://localhost:44369',
     production: false
 
 };
